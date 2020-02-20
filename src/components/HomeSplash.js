5d4e774fadf5af89858fdfafe9bc8b1f8e0063ab
Nick <barnard.nicholas@googlemail.com>
2020-02-20 11:44:01 +0000

    rough out home splash component

--- src/components/HomeSplash.js
+++ src/components/HomeSplash.js
@@ -2,23 +2,27 @@ import React from "react";
 import "./home_splash.css";
 import HomeSectionButton from "./HomeSectionButton";
 import whitelogo from "../assets/graphics/1080x270-03.png";
+import protein from "../assets/graphics/Protein_PIP_PDB_2icn.png";
 
 const HomeSplash = () => {
   return (
     <div id="homesplash">
-      <img src={whitelogo} alt="H1VE" id="logo" />
-      <h2>A boutique fitness studio like no other.</h2>
-      <p id="splashtext">
-        Combining cardio, functional, strength and conditioning training with
-        fantastic equipment, MYZONE technology, state of the art lighting, led
-        by Rockstar coaches, all of which add to the breathtaking experience
-        that is H1VE.
-      </p>
-      <HomeSectionButton
-        text="JOIN NOW"
-        backgroundColor="#0F0A59"
-        textColor="#ffffff"
-      />
+      <div id="splashcontainer">
+        <img src={whitelogo} alt="H1VE" id="logo" />
+        <h2>A boutique fitness studio like no other.</h2>
+        <p id="splashtext">
+          Combining cardio, functional, strength and conditioning training with
+          fantastic equipment, MYZONE technology, state of the art lighting, led
+          by Rockstar coaches, all of which add to the breathtaking experience
+          that is H1VE.
+        </p>
+        <HomeSectionButton
+          text="JOIN NOW"
+          backgroundColor="#0F0A59"
+          textColor="#ffffff"
+        />
+      </div>
+      <img src={protein} alt="graphic" id="proteingraphic" />
     </div>
   );
 };
