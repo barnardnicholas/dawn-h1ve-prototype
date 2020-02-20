7f6dab6a79ee2b6e35e82da3fb3862a3ba0a318c
Nick <barnard.nicholas@googlemail.com>
2020-02-20 12:06:50 +0000

    rough out book now component

--- src/components/HomeBookNow.js
+++ src/components/HomeBookNow.js
@@ -1,8 +1,35 @@
 import React from "react";
 import "./home_booknow.css";
+import HomeSectionButton from "./HomeSectionButton";
 
 const HomeBookNow = () => {
-  return <div id="homebooknow">BOOK NOW</div>;
+  return (
+    <div id="homebooknow">
+      <div id="booknowcontainer">
+        <div>
+          <h2>CLASSES</h2>
+          <p id="classestext">
+            The H1VE boutique fitness studio offers over 25 dynamic, energising
+            and motivating classes every week, each designed to push you to your
+            limits and give you a full body workout to remember! Classes are all
+            led by our ‘Rockstar’ coaches whose sessions combine cardio,
+            functional, strength and conditioning training. You will be part of
+            an environment like no other, with classes changing regularly to
+            ensure you continuously learn new training techniques and progress
+            your fitness.
+          </p>
+          <h3>
+            Book your free taster session today to experience H1VE for yourself.
+          </h3>
+          <HomeSectionButton
+            text="BOOK NOW"
+            backgroundColor="#F24423"
+            textColor="#ffffff"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeBookNow;
